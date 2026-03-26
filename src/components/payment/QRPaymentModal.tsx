4627520a39196a804b3d552bf4304664e60e9bac
collectivepalawan-oss <collectivepalawan@gmail.com>
2026-03-26 11:32:02 +0800

    Update QRPaymentModal.tsx

--- src/components/payment/QRPaymentModal.tsx
+++ src/components/payment/QRPaymentModal.tsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { QRCodeSVG } from 'qrcode.react';
-import { paymentService } from '@/services/paymentService';
+import { paymentService } from '@/paymentService';
 
 interface QRPaymentModalProps {
   isOpen: boolean;
